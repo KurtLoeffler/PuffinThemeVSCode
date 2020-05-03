292e31828f7a783be67ed6f4b38a1fcaf2cfd5ca
Kurt Loeffler <kurtloeffler@gmail.com>
2020-05-02 19:36:21 -0700

    commit message

---
+++ README.md
@@ -0,0 +1,9 @@
+## Puffin Theme
+
+### Install
+press ctl/command + shift + p to launch the command palette then run
+```
+ext install puffin-theme
+```
+
+![](images/screenshot1.png)
---
+++ build.lua
@@ -0,0 +1,41 @@
+
+local colors = {
+	{"base_1_1", "#24232a"},
+	{"base_1_2", "#2e2b31"},
+	{"base_1_3", "#3a3539"},
+	{"base_2_2", "#6b6264"},
+	{"green_3_2", "#b4c283"},
+	{"base_1", "#191a23"},
+	{"base_2", "#463f42"},
+	{"base_3", "#918686"},
+	{"base_4", "#ebebe1"},
+	{"beige_2", "#604d5c"},
+	{"beige_3", "#bc9c86"},
+	{"beige_4", "#eee0c4"},
+	{"red_2", "#45324b"},
+	{"red_3", "#d65676"},
+	{"red_4", "#f9c490"},
+	{"green_2", "#2f514c"},
+	{"green_3", "#879d70"},
+	{"green_4", "#e1e897"},
+	{"blue_2", "#243b4e"},
+	{"blue_3", "#4d95b1"},
+}
+
+local content
+do
+	local f = io.open("template-color-theme.json", "r")
+	content = f:read("*a")
+	f:close()
+end
+
+local outputContent = content
+for _, value in ipairs(colors) do
+	outputContent, count = string.gsub(outputContent, "%$"..value[1], value[2])
+end
+
+do
+	local f = io.open("themes/color-theme.json", "w")
+	f:write(outputContent)
+	f:close()
+end
